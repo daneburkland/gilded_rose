ea358d2f9f14a4b1005609f267aef6fd327382d8
Dane Burkland <dane.a.burkland@gmail.com>
2014-11-13 17:12:08 -0600

    Refactored ticket subclass

--- ticket_item.rb
+++ ticket_item.rb
@@ -6,14 +6,12 @@ class TicketItem < Item
       @quality = 0
     elsif @sell_in <= 5
       @quality += 3
-      @quality = [50, @quality].min
     elsif @sell_in <= 10
       @quality += 2
-      @quality = [50, @quality].min
     else
       @quality += 1
-      @quality = [50, @quality].min
     end
     @sell_in = @sell_in - 1
+    @quality = [50, @quality].min
   end
 end
